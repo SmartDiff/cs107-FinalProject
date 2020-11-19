040954c86158fff1cc44bd00b869a37b05cea61c
Xincheng Tan <caratan1998@yahoo.com>
2020-11-18 22:25:33 -0500

    minor changes

--- SmartDiff/preprocess/pyexpr_builder.py
+++ SmartDiff/preprocess/pyexpr_builder.py
@@ -1,7 +1,7 @@
 import sympy
 
 from SmartDiff.solvers.integrator import AutoDiffToy as AD
-import SmartDiff.solvers.element_op as el
+from SmartDiff.solvers.element_op import *
 
 # The evaluator takes as input a string representation of a math expression
 # and parses the string by iteratively building the AD object applied with
@@ -17,4 +17,5 @@ print(eval("math.log(4)"))
 s = "el.power(x, 2) * 5"
 x = AD(3)
 print(eval(s))
+
 
