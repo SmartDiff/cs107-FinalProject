04300da9213eed4510c1544ee17da190d1bedfa3
unknown <yilanwang@g.harvard.edu>
2020-11-19 02:02:54 -0500

    changed import in element_op

--- SmartDiff/solvers/element_op.py
+++ SmartDiff/solvers/element_op.py
@@ -1,4 +1,4 @@
-from integrator import AutoDiffToy as AD
+from SmartDiff.solvers.integrator import AutoDiffToy as AD
 import numpy as np
 
 # Here contains the derivative calculation of elementary operations
